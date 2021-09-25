fe8351d1673298da5a1a8e76a477cc768452295c
Pedro Victor <pedrovictor.rveras12@gmail.com>
2021-09-24 21:26:44 -0300

    quase finalizado, falta colocar js

---
+++ PVN Imoveis/JS/script.js
@@ -0,0 +1,8 @@
+function chamarWhatsapp() {
+
+    let mensagem;
+
+    mensagem = "Ola corretor Pedro Veras! gostaria de saber mais sobre os empreendimentos.";
+
+    window.open("https://wa.me/+5521997091499?text=" + mensagem)
+}
